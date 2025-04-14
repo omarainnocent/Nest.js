989f90d10d21e096478de2e9ea9e8a36a3996186
im <omarainnocent644@gmail.com>
2025-04-14 16:59:04 +0300

    any

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
-@Controller('users')
-export class UsersController {}
+@Controller('users')  
+export class UsersController {
+    /*
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+
+    @Get() // GET /users
+findAll() {
+    return []
+}
+}
+
