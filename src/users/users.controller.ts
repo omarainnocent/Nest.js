eff35248b28dfc7b8343df2f54b158194a251083
im <omarainnocent644@gmail.com>
2025-04-15 14:53:21 +0300

    method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,34 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Patch, Delete, Query } from '@nestjs/common';
 
 @Controller('users')  
 export class UsersController {
-    /*
-    GET /users/:id
-    POST /users
-    PATCH /users/:id
-    DELETE /users/:id
-    */
 
-    @Get() // GET /users
-findAll() {
+@Get() //GET / users or /users?role=value
+findAll(@Query('role') role?: 'INTER' | 'ENGINEER' | 'ADMIN')
+{
     return []
 }
+@Get(':id') // Get / user/: id
+findOne(@Param('id') id: string) {
+    return { id }
+
+}
+@Post() //POST /users
+create(@Body() user: {}) {
+    return user
+
 }
 
+@Patch(':id') // Patch /users/:id
+update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate }
+
+}
+
+@Delete(':id') // DELETE /users/:id
+delete(@Param('id') id: string) {
+    return { id }
+
+}
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": 'John Doe',
+            "email": "johndoe@gmail.com",
+            "role": 'INTER',
+        },
+        {
+            "id": 2,
+            "name": 'Jane Smith',
+            "email": 'janesmith@gmail.com',
+            "role": 'INTERN',
+        },
+        {
+            "id": 3,
+            name: 'Clementine Baoch',
+            "email": "clementinebaoch@gmail.com",
+            role: 'ENGINEER',
+        },
+        {
+            "id": 4,
+            "name": 'Patricia Lebsack',
+            "email": 'patriciaa@gmail.com',
+            "role": 'ENGINEER',
+        },
+        {
+            "id": 5,
+            "name": 'Chelsey Dietrich',
+            "email": 'chelseydaetrich@gmail.com',
+            "role": 'ADMIN',
+        },
+    ]
+
+    
+}
