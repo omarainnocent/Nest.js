342e67524466f0bc2390c341b7edc1df837588d6
im <omarainnocent644@gmail.com>
2025-04-16 14:36:21 +0300

    validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsEnum } from "class-validator";
 export class CreateUserDto {
     name: string;
+
+    @IsEmail()
     email: string;
+
+    @IsEnum(["INTERN", "ENGINEER", "ADMIN"], {
+        message: 'Valid role required'
+    })
     role: "INTERN" | "ENGINEER" | "ADMIN";
 }
